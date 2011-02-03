6c53ec2330012e2f2894d693019aa1d774c8cc4f
Ryan Kabir <rkabir@alum.mit.edu>
2011-02-03 01:33:38 -0400

    whitespace

--- lib/google-reader-api/api.rb
+++ lib/google-reader-api/api.rb
@@ -1,34 +1,34 @@
 module GoogleReaderApi
-  
+
   class Api
-    
+
     require "cgi"
     require "net/https"
     require "uri"
-    
+
     BASE_URL = "http://www.google.com/reader/"
-    
+
     def initialize(email,password)
       request_auth(email,password)
       @cache = GoogleReaderApi::Cache.new(2)
     end
-    
+
     # do a get request to the link
     # args is a hash of values that should be used in the request
     def get_link(link,args={})
       link = BASE_URL + link
       get_request(link,args)
     end
-    
+
     def post_link(link,args={})
       link = BASE_URL + link
       post_request(link,args)
     end
-    
+
     def cached_unread_count
       @cache['unread-count'] ||= get_link 'api/0/unread-count', :output => :json
     end
-    
+
     private
 
     # url as a string
@@ -44,10 +44,10 @@ module GoogleReaderApi
     # e.g. :allcomments => true etc...
     def get_request(url,args)
       uri = URI.parse url
-      
+
       # ck is the current unix timestamp
       args[:ck] = Time.now.to_i unless args[:ck]
-      
+
       req = Net::HTTP::Get.new("#{uri.path}?#{argument_string(args)}")
       request(uri,req)
     end
@@ -61,7 +61,7 @@ module GoogleReaderApi
       # TODO: use better exception
       if res.code != '200'
         p res.body
-        raise "something went wrong" 
+        raise "something went wrong"
       end
       res.body
     end
@@ -70,17 +70,17 @@ module GoogleReaderApi
     def argument_string(args)
       args.to_a.map { |v| v.join '=' }.join('&')
     end
-   
+
     def auth
       @auth
     end
-    
+
     def request_auth(email,password)
       login = GoogleLogin::ClientLogin.new service: 'reader', source: 'nudded-greader-0.1'
       login.authenticate email, password
       @auth = login.auth
     end
-    
+
   end
-  
+
 end
--- lib/google-reader-api/subscription_list.rb
+++ lib/google-reader-api/subscription_list.rb
@@ -1,22 +1,22 @@
 module GoogleReaderApi
-  
+
   class SubscriptionList
-    
+
     require "cgi"
-    
+
     include GoogleReaderApi::RssUtils
     include Enumerable
-    
+
     def initialize(api)
       @api = api
       update
     end
-    
+
     # returns the total unread count
     def total_unread
       inject(0) {|i,j| i+j.unread_count}
     end
-    
+
     # returns a hash
     # with following pattern:
     # feed => unread_count
@@ -25,26 +25,26 @@ module GoogleReaderApi
       each { |feed| hash[feed] = feed.unread_count }
       hash
     end
-     
+
     # yield each feed, if you return true
     # the feed will be removed from your subscriptions
     def remove_if
       each { |feed| feed.unsubscribe if yield feed}
       update
     end
-    
+
     # subscribe to the given url
     # google will set the title for you
     def add(url)
-      @api.post_link 'api/0/subscription/edit', :s => "feed/#{url}" , :ac => :subscribe 
+      @api.post_link 'api/0/subscription/edit', :s => "feed/#{url}" , :ac => :subscribe
       update
     end
-    
+
     # return an array of unread items
     def unread_items
       feeds.map(&:all_unread_items)
     end
-    
+
     # will return an array of entries with label
     def items_with_label(label)
       create_entries(@api.get_link "atom/user/-/label/#{CGI::escape label}")
@@ -57,18 +57,18 @@ module GoogleReaderApi
     def each
       @feeds.each {|feed| yield feed}
     end
-    
+
     def update
       fetch_list
     end
 
     private
-    
+
     def fetch_list
       json = JSON[@api.get_link 'api/0/subscription/list', :output => :json]['subscriptions']
       @feeds = json.map {|hash| GoogleReaderApi::Feed.new(hash,@api) }
     end
-    
+
   end
-  
+
 end
